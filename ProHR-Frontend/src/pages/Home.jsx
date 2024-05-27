e632b648f2a1eeb2de4bc964eb68f571561b06fc
Loganathanleo <logeshbigil8602@gmail.com>
2024-05-27 11:22:46 +0530

    Code Modified

--- ProHR-Frontend/src/pages/Home.jsx
+++ ProHR-Frontend/src/pages/Home.jsx
@@ -1,9 +1,41 @@
 import React from "react";
+import { Greeting } from "../components/greeting/greeting";
+import { Button, Table } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
-    return (
-        <div>Home</div>
-    );
+    const navigate = useNavigate();
+    const handleAdd = () => {
+        navigate("/Add");
+    };
+  return (
+    <div>
+      <div className="w-100">
+        <h1 xs={12} className="text-center mb-4">
+          <Greeting />
+        </h1>
+        <Button onClick={handleAdd}>Add Employee +</Button>
+      </div>
+      <div>
+        <Table>
+            <thead>
+                <tr>
+                    <th>Emp Name</th>
+                    <th>Degree</th>
+                    <th>Role</th>
+                    <th>Company Email</th>
+                    <th>Contact No</th>
+                    <th colSpan={2}>Actions</th>
+                    <th>Attendance</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr></tr>
+            </tbody>
+        </Table>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
--- ProHR-Frontend/src/routers/approuters.jsx
+++ ProHR-Frontend/src/routers/approuters.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "../pages/Login";
 import Home from "../pages/Home";
+import AddEmployee from "../components/Forms/AddEmployee";
 
 function AppRouters() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/Home" element={<Home />} />
+        <Route path="Home" element={<Home />} />
+        <Route path="Add" element={<AddEmployee />} />
       </Routes>
     </Router>
   );
