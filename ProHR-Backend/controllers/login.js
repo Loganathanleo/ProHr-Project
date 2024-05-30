de77d2d082194e954c6b85d8df56993e95b00ce6
Jayakanth <jayakanth1812@gmail.com>
2024-05-30 12:47:25 +0530

    added hashing password and removed env file

--- ProHR-Backend/controllers/login.js
+++ ProHR-Backend/controllers/login.js
@@ -1,14 +1,18 @@
 const Register = require("../models/register");
+const bcrypt = require("bcrypt");
 
 const loginController = async (req, res) => {
   const { email, password } = req.body;
   try {
-    const user = await Register.findOne({ email, password });
+    const user = await Register.findOne({ email });
     if (!user) {
-      return res.status(400).json("Invalid Credentials");
+      return res.status(400).json("No user found");
+    }
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) {
+      return res.status(400).json("Incorrect password");
     }
     res.json(user);
-    console.log("loginController successful");
   } catch (error) {
     console.error(error);
     res.status(500).send("Server error");
--- ProHR-Backend/controllers/register.js
+++ ProHR-Backend/controllers/register.js
@@ -1,14 +1,26 @@
 const Register = require("../models/register");
+const bcrypt = require("bcrypt");
 
 const registerController = async (req, res) => {
   const { name, dob, email, role, password, contactno } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
   try {
-    const user = new Register({
+    if (!name || !dob || !email || !role || !password || !contactno) {
+      return res.status(400).json("all fields are required");
+    }
+    if (password.length < 6) {
+      return res.status(400).json("password must be atleast 6 characters long");
+    }
+    const userExists = await Register.findOne({ email });
+    if (userExists) {
+      return res.status(400).json("user already exists");
+    }
+    const user = await Register.create({
       name,
       dob,
       email,
       role,
-      password,
+      password: hashedPassword,
       contactno,
     });
     await user.save();
